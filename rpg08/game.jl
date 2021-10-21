fe952b319441965bcff137e95fe8b850e06b015c
muuumin-soft <92678211+muuumin-soft@users.noreply.github.com>
2021-10-21 23:44:20 +0900

    第8回開始時点

---
+++ rpg08/game.jl
@@ -0,0 +1,30 @@
+module Game
+
+using Random
+import REPL
+using REPL.TerminalMenus
+
+include("キャラクター.jl")
+include("戦闘.jl")
+include("ui.jl")
+
+function main()
+    モンスター = Tモンスター("ドラゴン", 400, 80, 40, 10, [])
+    プレイヤー1 = Tプレイヤー("太郎", 100, 20, 10, 10, [createスキル(:大振り), createスキル(:連続攻撃)])
+    プレイヤー2 = Tプレイヤー("花子", 100, 20, 10, 10, [createスキル(:大振り), createスキル(:連続攻撃)])
+    プレイヤー3 = Tプレイヤー("遠藤君", 100, 20, 10, 10, [createスキル(:大振り), createスキル(:連続攻撃)])
+    プレイヤー4 = Tプレイヤー("高橋先生", 100, 20, 10, 10, [createスキル(:大振り), createスキル(:連続攻撃)])
+
+    println("モンスターに遭遇した！")
+    println("戦闘開始！")
+
+    ゲームループ([プレイヤー1, プレイヤー2, プレイヤー3, プレイヤー4], [モンスター])
+
+    if モンスター.HP == 0
+        println("戦闘に勝利した！")
+    else
+        println("戦闘に敗北した・・・")
+    end
+end
+
+end
---
+++ rpg08/スキル.jl
@@ -0,0 +1,48 @@
+abstract type T行動内容 end 
+abstract type Tスキル <: T行動内容　end 
+struct T通常攻撃 <: T行動内容 end
+struct Tかばう <: Tスキル end
+
+struct T攻撃スキル <: Tスキル
+    名前
+    威力
+    命中率
+    消費MP
+    攻撃回数min
+    攻撃回数max
+    T攻撃スキル(名前, 威力, 命中率, 消費MP, 攻撃回数min, 攻撃回数max) = begin
+        if 威力 < 0
+            throw(DomainError("威力が負の値になっています"))
+        end
+        if !(0 ≤ 命中率 ≤ 1)
+            throw(DomainError("命中率は0から1の間でなければなりません"))
+        end        
+        if 消費MP < 0
+            throw(DomainError("消費MPが負の値になっています"))
+        end 
+        if 攻撃回数min < 0
+            throw(DomainError("攻撃回数minが負の値になっています"))
+        end 
+        if 攻撃回数max < 0
+            throw(DomainError("攻撃回数maxが負の値になっています"))
+        end 
+        if 攻撃回数max < 攻撃回数min 
+            throw(DomainError("攻撃回数maxが攻撃回数minより小さくなっています"))
+        end 
+        new(名前, 威力, 命中率, 消費MP, 攻撃回数min, 攻撃回数max)  
+    end
+end
+
+function T攻撃スキル(名前, 威力, 命中率, 消費MP) 
+    return T攻撃スキル(名前, 威力, 命中率, 消費MP, 1, 1)
+end
+
+function createスキル(スキルシンボル)
+    if スキルシンボル == :大振り
+        return T攻撃スキル("大振り", 2, 0.4, 0)
+    elseif スキルシンボル == :連続攻撃
+        return T攻撃スキル("連続攻撃", 0.5, 1, 10, 2, 5)
+    else
+        Throw(DomainError("未定義のスキルが指定されました"))
+    end
+end
