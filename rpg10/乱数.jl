be794e69abd741f8728dc6078af79f7ec6785379
muuumin-soft <92678211+muuumin-soft@users.noreply.github.com>
2021-11-08 23:57:56 +0900

    第10回 スタブ用の"get乱数生成器"を作成

--- rpg10/乱数.jl
+++ rpg10/乱数.jl
@@ -5,3 +5,21 @@ function get乱数生成器()
         return rand()
     end
 end
+
+function get乱数生成器stub()
+    function 数列生成(c::Channel)
+        val = 0
+        while (true)
+            put!(c, float(val))
+            val += 1//10
+            if val == 1
+                val = 0
+            end
+        end
+    end
+    chnl = Channel(数列生成);
+
+    return function exec()
+        return take!(chnl)
+    end
+end
