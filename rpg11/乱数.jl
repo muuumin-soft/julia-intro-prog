c0444bdfb58c5f187708a3f381b812e5f1b692f5
muuumin-soft <92678211+muuumin-soft@users.noreply.github.com>
2021-11-09 23:20:03 +0900

    第11回 "get乱数生成stub"を使いやすくし、テストケースを改良

--- rpg11/乱数.jl
+++ rpg11/乱数.jl
@@ -7,18 +7,25 @@ function get乱数生成器()
 end
 
 function get乱数生成器stub()
+    return get乱数生成器stub([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+end
+
+function get乱数生成器stub(指定数列)
+    if length(指定数列) == 0
+        throw(DomainError("指定数列として1つ以上の要素を含むようにしてください"))
+    end
     function 数列生成(c::Channel)
-        val = 0
+        i = 1
         while (true)
-            put!(c, float(val))
-            val += 1//10
-            if val == 1
-                val = 0
+            put!(c, 指定数列[i])
+            i += 1
+            if length(指定数列) < i
+                i = 1
             end
         end
     end
     chnl = Channel(数列生成);
-
+    
     return function exec()
         return take!(chnl)
     end
