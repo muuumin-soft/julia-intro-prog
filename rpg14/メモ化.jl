e30a4c11ea919dec198703f0c4d38e74d4804848
muuumin-soft <92678211+muuumin-soft@users.noreply.github.com>
2021-11-29 23:10:10 +0900

    第14回 可変オブジェクトに対するメモ化

--- rpg14/メモ化.jl
+++ rpg14/メモ化.jl
@@ -1,12 +1,32 @@
+function メモ化用hash(x::Any)
+    return メモ化用hash(x, UInt(0))
+end
+
+function メモ化用hash(x, h::UInt)
+    return Base.hash(x, h)
+end
+
+function メモ化用hash_allargs(args...; kwargs...)
+    h = UInt(0)
+    for arg in args
+        h = メモ化用hash(arg, h)
+    end
+    for kwarg in kwargs
+        h = メモ化用hash(kwarg.first, h)
+        h = メモ化用hash(kwarg.second, h)
+    end
+    return h
+end
+
 function メモ化(func)
     memo = Dict()
     function メモ化されたfunc(args...; kwargs...)
-        allargs = (args, kwargs)
-        if haskey(memo, allargs)
-            return memo[allargs]
+        key = メモ化用hash_allargs(args...; kwargs...)
+        if haskey(memo, key)
+            return memo[key]
         end
         val = func(args...; kwargs...)
-        memo[allargs] = val
+        memo[key] = val
         return val
     end
 end
--- rpg14/メモ化_test.jl
+++ rpg14/メモ化_test.jl
@@ -2,6 +2,19 @@ include("メモ化.jl")
 
 using Test
 
+mutable struct メモ化テスト用構造体
+    数値
+    配列
+end
+
+function メモ化用hash(s::メモ化テスト用構造体, hsh::UInt)
+    #オブジェクト自体と内部のフィールドも組み合わせてのハッシュ
+    h = hsh
+    h = Base.hash(s, h)
+    h = メモ化用hash(s.数値, h)
+    h = メモ化用hash(s.配列, h)
+    return h
+end
 
 @testset "メモ化" begin
     function func(a, b, c...; d, e, f...)
@@ -22,4 +35,29 @@ using Test
     @test func(1, 2,       d=5, e=6          ) == m(1, 2,       d=5, e=6          ) == 14 #可変長引数なし
     @test func(1, 2, 3,    d=5, e=6, f=7     ) == m(1, 2, 3,    d=5, e=6, f=7     ) == 24 #可変長引数1個
     @test func(1, 2, 3, 4, d=5, e=6, f=7, g=8) == m(1, 2, 3, 4, d=5, e=6, f=7, g=8) == 36 #可変長引数2個
+
+    @testset "可変オブジェクトの中身が変わったとき、不要なメモを参照しないこと" begin
+        @testset "配列" begin
+            f(x) = sum(x)
+            g = メモ化(f)
+            a = [1, 2]
+            @test g(a) == 3
+            push!(a, 3) #[1, 2, 3]
+            @test g(a) == 6
+            a[1] = 10 #[10, 2, 3]
+            @test g(a) == 15
+        end
+        @testset "構造体" begin
+            f(x) = x.数値 + sum(x.配列)
+            g = メモ化(f)
+            s = メモ化テスト用構造体(1, [2, 3])
+            @test g(s) == 6
+            s.数値 = 2 #(2, [2, 3])
+            @test g(s) == 7
+            s.配列[1] = 20 #(2, [20, 3])
+            @test g(s) == 25
+            s.配列 = [10, 20] #(2, [10, 20])
+            @test g(s) == 32
+        end
+    end
 end
